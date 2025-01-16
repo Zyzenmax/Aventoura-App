17d97a0705af101fd247ccf16858e12372310cbe
Zyzenbeep <bagushasanali78@gmail.com>
2025-01-16 20:48:53 +0700

    Show the bottom part of the ticket

--- ticket_app/lib/base/widgets/ticket_view.dart
+++ ticket_app/lib/base/widgets/ticket_view.dart
@@ -19,81 +19,178 @@ class TicketView extends StatelessWidget {
       height: 197,
       child: Container(
         margin: const EdgeInsets.only(right: 16),
-        padding: EdgeInsets.all(16),
-
-        // ignore: sort_child_properties_last
         child: Column(
           children: [
-            Row(
-              children: [
-                Text(
-                  "NYC",
-                  style: AppStyles.headlinestyle2.copyWith(color: Colors.white),
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                const BigDot(),
-                Expanded(
-                  child: Stack(
+            Container(
+              padding: EdgeInsets.all(16),
+
+              // ignore: sort_child_properties_last
+              child: Column(
+                children: [
+                  Row(
                     children: [
-                      const SizedBox(
-                        height: 23.5,
-                        child: AppLayoutBuilderWidget(
-                          randomDivider: 6,
+                      Text(
+                        "NYC",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      const BigDot(),
+                      Expanded(
+                        child: Stack(
+                          children: [
+                            const SizedBox(
+                              height: 23.5,
+                              child: AppLayoutBuilderWidget(
+                                randomDivider: 6,
+                              ),
+                            ),
+                            Center(
+                                child: Transform.rotate(
+                                    angle: 1.57,
+                                    child: const Icon(
+                                      Icons.local_airport_rounded,
+                                      color: Colors.white,
+                                    ))),
+                          ],
                         ),
                       ),
-                      Center(
-                          child: Transform.rotate(
-                              angle: 1.57,
-                              child: const Icon(
-                                Icons.local_airport_rounded,
-                                color: Colors.white,
-                              ))),
+                      const BigDot(),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        "LDN",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      )
                     ],
                   ),
-                ),
-                const BigDot(),
-                Expanded(
-                  child: Container(),
-                ),
-                Text(
-                  "LDN",
-                  style: AppStyles.headlinestyle2.copyWith(color: Colors.white),
-                )
-              ],
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "New-York",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        "8H 30M ",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        "London",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(
+                  color: AppStyles.ticketBlue,
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(21),
+                      topRight: Radius.circular(21))),
             ),
-            SizedBox(
-              height: 5,
-            ),
-            Row(
-              children: [
-                Text(
-                  "New-York",
-                  style: AppStyles.headlinestyle2.copyWith(color: Colors.white),
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Text(
-                  "8H 30M ",
-                  style: AppStyles.headlinestyle2.copyWith(color: Colors.white),
-                ),
-                Expanded(
-                  child: Container(),
-                ),
-                Text(
-                  "London",
-                  style: AppStyles.headlinestyle2.copyWith(color: Colors.white),
-                )
-              ],
+
+            //Container 2
+            Container(
+              padding: EdgeInsets.all(16),
+
+              // ignore: sort_child_properties_last
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "NYC",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      const BigDot(),
+                      Expanded(
+                        child: Stack(
+                          children: [
+                            const SizedBox(
+                              height: 23.5,
+                              child: AppLayoutBuilderWidget(
+                                randomDivider: 6,
+                              ),
+                            ),
+                            Center(
+                                child: Transform.rotate(
+                                    angle: 1.57,
+                                    child: const Icon(
+                                      Icons.local_airport_rounded,
+                                      color: Colors.white,
+                                    ))),
+                          ],
+                        ),
+                      ),
+                      const BigDot(),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        "LDN",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "New-York",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        "8H 30M ",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        "London",
+                        style: AppStyles.headlinestyle2
+                            .copyWith(color: Colors.white),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(21),
+                      bottomRight: Radius.circular(21))),
             )
           ],
         ),
-        decoration: BoxDecoration(
-            color: AppStyles.ticketBlue,
-            borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(21), topRight: Radius.circular(21))),
       ),
     );
   }
